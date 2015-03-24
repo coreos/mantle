dd99041d5a1b81e8d3fb89fc893194d35b63c5b5
Patrick Baxter <pb@coreos.com>
2015-03-24 19:05:42 -0400

    cmd/kola/etcdtests: add util file for shared etcd functionality
    Discovery test has been refactored to use the new util file. Most
    utility functions will be used in upcoming etcd migration and fallback
    tests.

--- cmd/kola/etcdtests/etcdDiscovery.go
+++ cmd/kola/etcdtests/etcdDiscovery.go
@@ -3,8 +3,6 @@ package etcdtests
 import (
 	"fmt"
 	"os"
-	"strings"
-	"time"
 
 	"github.com/coreos/mantle/platform"
 )
@@ -19,52 +17,18 @@ func etcdDiscovery(cluster platform.Cluster) error {
 		}
 	}
 
-	// point etcd on each machine to discovery
-	for i, m := range cluster.Machines() {
-		// start etcd instance
-		etcdStart := "sudo systemctl start etcd.service"
-		_, err := m.SSH(etcdStart)
-		if err != nil {
-			return fmt.Errorf("SSH cmd to %v failed: %s", m.IP(), err)
-		}
-		fmt.Fprintf(os.Stderr, "etcd instance%d started\n", i)
+	// start etcd cluster
+	err = startEtcd(cluster)
+	if err != nil {
+		return err
 	}
 
-	err := getClusterHealth(cluster.Machines()[0], csize)
+	// check health on first machine
+	err = getClusterHealth(cluster)
 	if err != nil {
-		return fmt.Errorf("Discovery failed health check: %v", err)
+		return fmt.Errorf("discovery failed health check: %v", err)
 	}
 
 	fmt.Fprintf(os.Stderr, "etcd Discovery succeeeded!\n")
 	return nil
-}
-
-// poll cluster-health until result
-func getClusterHealth(m platform.Machine, csize int) error {
-	const (
-		retries   = 5
-		retryWait = 3 * time.Second
-	)
-	var err error
-	var b []byte
-
-	for i := 0; i < retries; i++ {
-		fmt.Fprintf(os.Stderr, "polling cluster health...\n")
-		b, err = m.SSH("etcdctl cluster-health")
-		if err == nil {
-			break
-		}
-		time.Sleep(retryWait)
-	}
-	if err != nil {
-		return fmt.Errorf("health polling failed: %s", b)
-	}
-
-	// repsonse should include "healthy" for each machine and for cluster
-	if strings.Count(string(b), "healthy") == csize+1 {
-		fmt.Fprintf(os.Stderr, "%s\n", b)
-		return nil
-	} else {
-		return fmt.Errorf("Status unhealthy or incomplete: %s", b)
-	}
 }
